3514cb35a52f50e22ff8e3bc8604ea1beddbe4fb
16yuki0702 <hs19870702@gmail.com>
2021-07-15 13:58:38 +0900

    Add cases

--- lib.cc
+++ lib.cc
@@ -908,3 +908,47 @@ public:
         return -1;
     }
 };
+
+// 34. Find First and Last Position of Element in Sorted Array
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                if (nums[left] < target) {
+                    left++;
+                }
+                if (nums[right] > target) {
+                    right--;
+                }
+                if (nums[left] == nums[right]) {
+                    return {left, right};
+                }
+            }
+        }
+        return {-1, -1};
+    }
+};
+
+// 35. Search Insert Position
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0, right = nums.size();
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] >= target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
