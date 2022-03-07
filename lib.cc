262442fb67162d2adf0e1064ee5e72317363b603
16yuki0702 <hs19870702@gmail.com>
2022-03-07 11:04:34 +0900

    Add cases

--- lib.cc
+++ lib.cc
@@ -3700,3 +3700,38 @@ public:
         return res;
     }
 };
+
+// 138. Copy List with Random Pointer
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        map<Node*, Node*> m;
+        Node* p = head;
+        while (p) {
+            m[p] = new Node(p->val);
+            p = p->next;
+        }
+        p = head;
+        while (p) {
+            m[p]->next = m[p->next];
+            m[p]->random = m[p->random];
+            p = p->next;
+        }
+        return m[head];
+    }
+};
