d0bd751977826742e518cc3ef897eaf52c108654
16yuki0702 <hs19870702@gmail.com>
2022-09-05 10:45:27 +0900

    Add cases

--- lib.cc
+++ lib.cc
@@ -8522,3 +8522,48 @@ public:
         recursive(r->left, sum);
     }
 };
+
+// 539. Minimum Time Difference
+class Solution {
+public:
+    int findMinDifference(vector<string>& timePoints) {
+        int res = INT_MAX, n = timePoints.size();
+        sort(timePoints.begin(), timePoints.end());
+        for (int i = 0; i < timePoints.size(); i++) {
+            int diff = abs(timeDiff(timePoints[(i - 1 + n) % n], timePoints[i]));
+            diff = min(diff, 1440 - diff);
+            res = min(res, diff);
+        }
+        return res;
+    }
+    int timeDiff(string s1, string s2) {
+        int h1 = stoi(s1.substr(0, 2));
+        int m1 = stoi(s1.substr(3, 2));
+        int h2 = stoi(s2.substr(0, 2));
+        int m2 = stoi(s2.substr(3, 2));
+        return (h2 - h1) * 60 + (m2 - m1);
+    }
+};
+
+// 540. Single Element in a Sorted Array
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int res = 0;
+        for (auto n : nums) {
+            res ^= n;
+        }
+        return res;
+    }
+};
+
+// 541. Reverse String II
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        for (int i = 0; i < s.size(); i += 2 * k) {
+            reverse(s.begin() + i, min(s.begin() + i + k, s.end()));
+        }
+        return s;
+    }
+};
