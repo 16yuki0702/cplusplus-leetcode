80fd29e2d29ce526ad9c954e2c236c68d5b408ab
16yuki0702 <hs19870702@gmail.com>
2022-11-28 10:48:24 +0900

    Add cases

--- lib.cc
+++ lib.cc
@@ -9664,3 +9664,32 @@ public:
 //     }
 // };
 
+// 669. Trim a Binary Search Tree
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* trimBST(TreeNode* root, int low, int high) {
+        if (!root) {
+            return root;
+        }
+        if (low <= root->val && root->val <= high) {
+            root->left = trimBST(root->left, low, high);
+            root->right = trimBST(root->right, low, high);
+            return root;
+        }
+        if (root->val < low) {
+            return trimBST(root->right, low, high);
+        }
+        return trimBST(root->left, low, high);
+    }
+};
