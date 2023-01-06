433c9ae5c072efc51ca1caa946124fb3788ed0ba
16yuki0702 <hs19870702@gmail.com>
2023-01-06 23:30:13 +0900

    Add cases

--- lib.cc
+++ lib.cc
@@ -10480,3 +10480,71 @@ public:
         flood(image, sr, sc - 1, color, target);
     }
 };
+
+// 738. Monotone Increasing Digits
+class Solution {
+public:
+    int monotoneIncreasingDigits(int n) {
+        string n_str = to_string(n);
+        int marker = n_str.size();
+        for (int i = n_str.size() - 1; i > 0; i--) {
+            if (n_str[i] < n_str[i - 1]) {
+                marker = i;
+                n_str[i - 1] = n_str[i - 1] - 1;
+            }
+        }
+        for (int i = marker; i < n_str.size(); i++) {
+            n_str[i] = '9';
+        }
+        return stoi(n_str);
+    }
+};
+
+// 739. Daily Temperatures
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int> res(temperatures.size(), 0);
+        stack<int> s;
+        for (int i = temperatures.size() - 1; i >= 0; i--) {
+            while (!s.empty() && temperatures[i] >= temperatures[s.top()]) {
+                s.pop();
+            }
+            if (!s.empty()) {
+                res[i] = s.top() - i;
+            }
+            s.push(i);
+        }
+        return res;
+    }
+};
+
+// 709. To Lower Case
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for (char& c : s) {
+            if ('A' <= c && c <= 'Z') {
+                c += 32;
+            }
+        }
+        return s;
+    }
+};
+
+// 744. Find Smallest Letter Greater Than Target
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int left = 0, right = letters.size() - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (letters[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return letters[left] > target ? letters[left] : letters[0];
+    }
+};
