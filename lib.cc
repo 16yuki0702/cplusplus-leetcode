afde0a8c0f0b769e221bc1cb46719e99fce3b456
16yuki0702 <hs19870702@gmail.com>
2023-01-12 21:16:55 +0900

    Add cases

--- lib.cc
+++ lib.cc
@@ -10649,3 +10649,18 @@ public:
         return true;
     }
 };
+
+// 766. Toeplitz Matrix
+class Solution {
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+        for (int r = 1; r < matrix.size(); r++) {
+            for (int c = 1; c < matrix[0].size(); c++) {
+                if (matrix[r][c] != matrix[r - 1][c - 1]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
